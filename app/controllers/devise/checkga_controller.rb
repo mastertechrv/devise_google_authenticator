6ef2c9cf0a75f419c4f4be2e5048af53c2b94101
Austin Klenk <austin@mastertechrv.com>
2016-12-29 10:27:44 -0500

    no layout

--- app/controllers/devise/checkga_controller.rb
+++ app/controllers/devise/checkga_controller.rb
@@ -1,9 +1,9 @@
 class Devise::CheckgaController < Devise::SessionsController
   prepend_before_filter :devise_resource, :only => [:show]
   prepend_before_filter :require_no_authentication, :only => [ :show, :update ]
-  render :layout => false
 
   include Devise::Controllers::Helpers
+  render :layout => false, :only => [:show]
 
   def show
     @tmpid = params[:id]
